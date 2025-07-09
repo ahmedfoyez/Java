1626d60da0e2e25cb4a444d65a649deccdb7ff03
Ahmed Foyez <93567821+ahmedfoyez@users.noreply.github.com>
2025-07-09 16:23:53 +0600

    Add files via upload

---
+++ EvenOdd.java
@@ -0,0 +1,25 @@
+<<<<<<< HEAD
+import java.util.Scanner;
+
+public class EvenOdd {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+
+=======
+import java.util.Scanner;
+
+public class EvenOdd {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+
+        if (number % 2 == 0) {
+            System.out.println(number + " is even.");
+        } else {
+            System.out.println(number + " is odd.");
+        }
+    }
+}
+>>>>>>> c9a2d49 (Add files via upload)
