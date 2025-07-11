fc5d1a9434bf1805681c95daa024ba9267bae984
Ahmed Foyez <93567821+ahmedfoyez@users.noreply.github.com>
2025-07-11 15:42:24 +0600

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,8 @@
+package inheritance;
+
+public class Test {
+    public static void main(String[] args) {
+        Teacher t1 = new Teacher("Faizur", 30, "Internship");
+        t1.displayInformation();
+    }
+}
