97602392bdb388ef924a33c1edc5d379572ed022
Ahmed Foyez <93567821+ahmedfoyez@users.noreply.github.com>
2025-07-11 19:01:13 +0600

    Add files via upload

---
+++ polymorphism/Student.java
@@ -0,0 +1,7 @@
+package polymorphism;
+public class Student extends Person{
+    @Override
+    void display(){
+        System.out.println("I am a Student");
+    }
+}
---
+++ polymorphism/Teacher.java
@@ -0,0 +1,7 @@
+package polymorphism;
+ public class Teacher extends Person{
+     @Override
+     void display(){
+         System.out.println("I am a Teacher");
+     }
+ }
